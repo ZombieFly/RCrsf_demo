747734e43d15dec0d7ec034524a21ed335c6c406
ZombieFly <xyzomfly@gmail.com>
2026-03-13 21:50:59 +0800

    🦄 refactor: 启动rcrsf DMA内存优化宏

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -46,7 +46,7 @@ extern "C" {
 
 /* Exported macro ------------------------------------------------------------*/
 /* USER CODE BEGIN EM */
-
+#define DMA_RAM_SECTION ".RAM_D2_Section"
 /* USER CODE END EM */
 
 /* Exported functions prototypes ---------------------------------------------*/
