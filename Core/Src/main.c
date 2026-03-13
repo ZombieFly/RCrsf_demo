0447713bf6fdc3720e4cf980d9c889dd84a3e24e
ZombieFly <xyzomfly@gmail.com>
2026-03-13 18:03:52 +0800

    ✨ feat: 内存分配
    主RAM使用DTCMRAM。RAM D2区域予以不缓存，并分配给DMA

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -210,6 +210,16 @@ void MPU_Config(void)
   MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
 
   HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+  /** Initializes and configures the Region and the memory to be protected
+  */
+  MPU_InitStruct.Number = MPU_REGION_NUMBER2;
+  MPU_InitStruct.BaseAddress = 0x30000000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_32KB;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
   /* Enables the MPU */
   HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
 
